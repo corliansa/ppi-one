74fc609a34aa48e8265191d3b9570bf9c9722749
Corliansa Kusumah <corliansa@gmail.com>
2022-07-24 15:37:04 +0200

    fix api url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,12 +19,9 @@ export default withTRPC<AppRouter>({
 		 * If you want to use SSR, you need to use the server's full URL
 		 * @link https://trpc.io/docs/ssr
 		 */
-		const url = process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}/api/trpc`
-			: "http://localhost:3000/api/trpc";
 
 		return {
-			url,
+			url: "/api/trpc",
 			transformer: superjson,
 			/**
 			 * @link https://react-query.tanstack.com/reference/QueryClient
