1982556789ee041d1b29ec560843cce14f4a6ba9
Corliansa Kusumah <corliansa@gmail.com>
2022-07-24 19:10:30 +0200

    added role to user

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,10 +1,20 @@
 import { createRouter } from "../createRouter";
 import superjson from "superjson";
 import { z } from "zod";
+import * as trpc from "@trpc/server";
 
 export const appRouter = createRouter()
 	.transformer(superjson)
 	.query("hello", { resolve: () => "Hello, World!" })
+	.middleware(async ({ ctx, next }) => {
+		if (!ctx.user.role.includes("admin")) {
+			throw new trpc.TRPCError({
+				code: "UNAUTHORIZED",
+				message: "You are not authorized to access this resource.",
+			});
+		}
+		return await next();
+	})
 	.query("getLinks", {
 		resolve: async ({ ctx: { user, prisma } }) => {
 			const links = await prisma.links.findMany({
@@ -17,8 +27,8 @@ export const appRouter = createRouter()
 	.mutation("createLink", {
 		input: z.object({
 			description: z.string().optional(),
-			url: z.string(),
-			slug: z.string(),
+			url: z.string().min(3).max(2048),
+			slug: z.string().min(1).max(2048),
 		}),
 		resolve: async ({
 			input: { description, url, slug },
